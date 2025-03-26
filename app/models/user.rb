da040fba350a38a8d7360cbdb61807b6699c43bb
Andrew Porter <partydrone@icloud.com>
2025-03-26 12:28:03 -0500

    Add user model unit tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   has_many :sessions, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  validates :email_address, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should be valid" do
+    user = User.new(email_address: "test@example.com", password: "password")
+    assert user.valid?
+  end
+
+  test "email address is required" do
+    user = User.new(password: "password")
+    user.email_address = nil
+    assert_not user.valid?
+    assert_includes user.errors[:email_address], "can't be blank"
+  end
+
+  test "email address must be unique" do
+    User.create(email_address: "test@example.com", password: "password")
+    user = User.new(email_address: "test@example.com", password: "password")
+    assert_not user.valid?
+    assert_includes user.errors[:email_address], "has already been taken"
+  end
+
+  test "email address must be valid" do
+    user = User.new(email_address: "test", password: "password")
+    assert_not user.valid?
+    assert_includes user.errors[:email_address], "is invalid"
+  end
 end
